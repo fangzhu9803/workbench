dd58bef72c85151c4f892e8a691c9f9dc5b0fee1
fangzhu9803 <fangzhu9803@users.noreply.github.com>
2026-07-29 12:14:33 +0800

    我的工作台 - 个人每日习惯追踪 PWA

---
+++ sw.js
@@ -0,0 +1,31 @@
+const C = "workbench-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).catch(() => {}));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== C).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(C).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
